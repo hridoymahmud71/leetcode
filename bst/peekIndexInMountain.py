3dfad243467883ad73029d625e51f9f233330102
hridoymahmud71 <hridoymahmud71@gmail.com>
2022-06-12 15:19:13 +0600

    update

--- bst/peekIndexInMountain.py
+++ bst/peekIndexInMountain.py
@@ -3,19 +3,21 @@ from typing import List
 
 class Solution:
     def peakIndexInMountainArray(arr: List[int]) -> int:
-        l = 0
-        r = len(arr) - 1
+        l , r =  0 , len(arr) - 1
         
-        while l < r:
-            m = l + r // 2
+        if r < 2:
+            return -1
+        
+        while l < r :
+            m =  (l + r) // 2
             
-            if(arr[m] < arr[m+1]):
+            if arr[m] < arr[m + 1] :
                 l = m + 1
             else:
                 r = m
         return l
 
-arr = [0,1]
+arr =  [3,4,5,1]
 sol = Solution.peakIndexInMountainArray(arr);
 
 print(sol);
