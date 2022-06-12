469b188c874f94747b6bd2d53a1a0c5c7fa8b87d
hridoymahmud71 <hridoymahmud71@gmail.com>
2022-06-12 16:06:29 +0600

    perfect square

---
+++ bst/isPerfectSquare.py
@@ -0,0 +1,24 @@
+
+
+from operator import truediv
+
+
+class Solution:
+    def isPerfectSquare(num:int)->bool:
+        l,r = 0,num
+
+        while l<=r:
+            m = l + (r-l) // 2
+            print("begin",m,l,r)
+            if m**2 == num:
+                return True
+            elif m**2 < num:
+                l = m + 1;
+            else:
+                r = m - 1;
+            print("end",m,l,r)
+        return False
+
+num = 16
+sol = Solution.isPerfectSquare(num)
+print(sol)
