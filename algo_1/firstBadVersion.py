c70aaffc6f35d85e535320a7b053c680ba232fd1
hridoymahmud71 <hridoymahmud71@gmail.com>
2022-06-14 17:45:44 +0600

    maximum sub array and first bad version

---
+++ algo_1/firstBadVersion.py
@@ -0,0 +1,29 @@
+# https://leetcode.com/problems/first-bad-version/
+import time
+
+import random
+
+class Solution:
+    def firstBadVersion(self,n:int) -> int:
+
+        #the isBadVersion method will be predefined in the api and defination will not be needed in leetcode submission
+        def isBadVersion(x):
+            return random.random() > .85
+
+        low,high = 0,n
+        
+
+        while low < high:
+            mid = low + (high - low) // 2
+            if(isBadVersion(mid)):
+                high = mid
+            else : 
+                low = mid + 1
+        return low
+
+# driver code 
+sol = Solution()
+start_time = time.time()
+res = sol.firstBadVersion(17000000000000000000)
+
+print(res,time.time()-start_time)
--- data_structure_1/maximumSubArray.py
+++ data_structure_1/maximumSubArray.py
@@ -22,8 +22,9 @@ class Solution :
         # 2. max subarray of right half
         # 3. max subarray sum such that the subarray crosses the midpoint
 
-        print(low,mid,high)
+    
         max_sum = max(self.maxSubArraySum(arr,low,mid),self.maxSubArraySum(arr,mid + 1,high),self.maxCrossingSum(arr, low, mid, high))
+    
         #return this largest possible sum of the section recursively
         return max_sum
 
@@ -37,19 +38,19 @@ class Solution :
         #left part
         for i in range(mid,low-1,-1):
             left_temp_sum = left_temp_sum + arr[i]
-            if(left_sum > left_temp_sum):
+            if(left_temp_sum > left_sum):
                 left_sum = left_temp_sum
         
         #right part
-        for i in range(mid+1,high + 1,):
+        for i in range(mid+1,high + 1):
             right_temp_sum = right_temp_sum + arr[i]
-            if(right_sum > right_temp_sum):
+            if(right_temp_sum > right_sum):
                 right_sum = right_temp_sum
 
         return max(left_sum,right_sum,left_sum + right_sum)
 
 # driver code
-arr = [2, 3, 4, 5, 10]
+arr = [5,4,-1,7,8]
 solution = Solution()
 result = solution.maxSubArray(arr)
 print(result)
