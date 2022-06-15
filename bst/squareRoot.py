6cff88fdf274aa3774251f3842a4f1479fc749d9
hridoymahmud71 <hridoymahmud71@gmail.com>
2022-06-15 18:57:07 +0600

    more changes

--- bst/squareRoot.py
+++ bst/squareRoot.py
@@ -1,5 +1,8 @@
 # https://leetcode.com/problems/sqrtx/
 
+import math 
+
+
 class Solution:
     def mySqrt(self, x: int) -> int:
         
@@ -8,38 +11,40 @@ class Solution:
         ans = 0
        
         # divide and conqure   
-        # while start < end :
-        #     mid = start + (end - start) // 2
+        while start < end :
+            mid = start + (end - start) // 2
             
-        #     print('start loop')
-        #     print(start,end,"||",mid,mid*mid)
+            print('start loop')
+            print(start,end,"||",mid,mid*mid)
             
-        #     if mid * mid == x :
-        #         return mid
-        #     elif mid * mid < x :
-        #         start = mid + 1
-        #         ans = start
-        #     else :
-        #         end = mid - 1
-        #         ans = end
-        #     print(ans)
-        #     print('end loop\n')
+            if mid * mid == x :
+                return mid
+            elif mid * mid < x :
+                start = mid + 1
+                ans = start
+            else :
+                end = mid - 1
+                ans = end
+            print(ans)
+            print('end loop\n')
+        # divide and conqure ends
 
+        
         # brute force
-        if x > 2:
-            rangeEnd = x
-            for i in(range(1,rangeEnd)):
-                isquare = i * i
-                if isquare == x:
-                    ans = i
-                    break
-                if(isquare > x):
-                    ans = i -1
-                    break
-        elif x == 1 or x == 2:
-            ans = 1
-        else :
-            ans = 0 
+        # if x > 2:          
+        #     for i in(range(1,x)):
+        #         isquare = i * i
+        #         if isquare == x:
+        #             ans = i
+        #             break
+        #         if(isquare > x):
+        #             ans = i -1
+        #             break
+        # elif x == 1 or x == 2:
+        #     ans = 1
+        # else :
+        #     ans = 0 
+        #brute force ends 
             
                 
         return ans
@@ -55,6 +60,7 @@ while True:
 
     sol =  Solution()
     res = sol.mySqrt(i)
-    print('ans is: ',res)      
+    actual_result = int(math.sqrt(i))
+    print('ans is: ',res, ' the actual result is ',actual_result ,'Answer is ',res == actual_result)      
 
 print("program ends")
