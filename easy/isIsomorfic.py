89cf76d5b255f776e926e484def4ef1074cb5a02
Mahmudur Rahman <hridoymahmud71@gmail.com>
2022-08-31 10:41:25 +0600

    Create isIsomorfic.py

---
+++ easy/isIsomorfic.py
@@ -0,0 +1,49 @@
+# https://leetcode.com/problems/isomorphic-strings/
+
+from ast import Not
+from operator import truediv
+
+
+class Solution:
+    def isIsomorphic(self, s: str, t: str) -> bool:
+        stMap, tsMap = {}, {}
+
+        for i in range(len(s)):
+
+            # 012   
+            # foo
+            # bar
+            # on oth and 1th iteration the stMap = {f:b,o:a} tsMap = {b:f,a:o}
+
+            # s[2] here is 'o' and it's already pushed in stMap but stMap[s[2]] / stMap[o] is r 'o->r'
+            # t[2] here is 'r' and it's not pushed in tsMap but tsMap[t[2]] / tsMap[r] is o 'r->o'
+            # if it's reversely mapping is should already be in the existing map but it's not. So it's a false case
+
+
+            # 012   
+            # foo
+            # foo
+           # on oth and 1th iteration the stMap = {f:f,o:ao} tsMap = {f:f,o:o}
+
+            # s[2] here is 'o' and it's already pushed in stMap but stMap[s[2]] / stMap[o] is r 'o->o'
+            # t[2] here is 'o' and it's aalready pushed in tsMap but tsMap[t[2]] / tsMap[r] is o 'o->o'
+            # if it's reversely mapping is should already be in the existing map but it's not. So it's a false case
+
+            if s[i] in stMap and stMap[s[i]] != t[i]:
+                return False
+
+            if t[i] in tsMap and tsMap[t[i]] != s[i]:
+                return False
+
+            # setting up the map
+            stMap[s[i]] = t[i]
+            tsMap[t[i]] = s[i]
+
+        return True
+
+# driver code
+s = "foo"
+t = "foo"
+sol = Solution()
+res = sol.isIsomorphic(s, t)
+print("solution is ", res, type(res))
