2c1e8b392ccc7f264c895c85285ba47c3c72e0fc
Mahmudur Rahman <hridoymahmud71@gmail.com>
2022-09-07 00:47:38 +0600

    Update sourroundedRegions.py

--- dp/sourroundedRegions.py
+++ dp/sourroundedRegions.py
@@ -13,26 +13,40 @@ class Solution:
 
         def traverseDFS(r, c):
 
-            if (r < 1 or r >= rows -1 or c < 1 or c >= cols - 1 or board[r][c] != "O"):
-                return
+            if (r < 0 or r >= rows  or c < 0 or c >= cols  or board[r][c] != "O"):
+                return            
                       
-            board[r][c] == "X"
+            board[r][c] = "-"
 
             traverseDFS(r, c-1)  # left
             traverseDFS(r, c+1)  # right
             traverseDFS(r-1, c)  # top
             traverseDFS(r+1, c)  # bottom
 
+        # Mofify the edge "O"s into "-"s
         for r in range(rows):
             for c in range(cols):
-                if board[r][c] == "O":
+                if board[r][c] == "O" and ( r == 0 or r == rows - 1 or c == 0 or c == cols -1):
                     traverseDFS(r, c)
 
-        return board
+       
+        # Flip
+        for r in range(rows):
+            for c in range(cols):
+                if board[r][c] == "O": 
+                    board[r][c] = "X"
+
+        for r in range(rows):
+            for c in range(cols):
+                if board[r][c] == "-": 
+                    board[r][c] = "O"
+
+        
+        print(board)
+
 
 
 # driver code
 board = [["X","X","X","X"],["X","O","O","X"],["X","X","O","X"],["X","O","X","X"]]
 sol = Solution()
-res = sol.numIslands(board)
-print("solution is ", res)
+res = sol.solve(board)
