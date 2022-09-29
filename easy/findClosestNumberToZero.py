518cfe4e51c786bd97ad67e01f2698fa864fab37
Mahmudur Rahman <hridoymahmud71@gmail.com>
2022-09-30 01:14:19 +0600

    Update findClosestNumberToZero.py

--- easy/findClosestNumberToZero.py
+++ easy/findClosestNumberToZero.py
@@ -1,23 +1,37 @@
 # https://leetcode.com/problems/find-closest-number-to-zero/submissions/
+from cmath import inf
 from typing import List
 
 
 class Solution:
     def findClosestNumber(self, nums: List[int]) -> int:
-        nums.sort()
-        mymin = abs(nums[0])
-        targetIndex = 0
+        # Solution with sorting starts
+        # nums.sort()
+        # mymin = abs(nums[0])
+        # targetIndex = 0
         
         
-        for i in range(1,len(nums)):
+        # for i in range(1,len(nums)):
            
-            if abs(nums[i]) <= mymin and nums[i] > nums[targetIndex]:
-                mymin = abs(nums[i])
-                targetIndex = i
+        #     if abs(nums[i]) <= mymin and nums[i] > nums[targetIndex]:
+        #         mymin = abs(nums[i])
+        #         targetIndex = i
                 
                 
             
-        return nums[targetIndex]
+        # return nums[targetIndex]
+        # Solution with sorting ends
+
+        output = float(inf)
+
+        for i in range(len(nums)):
+            if abs(nums[i]) < abs(output):
+                output  = nums[i]
+            elif abs(nums[i]) == abs(output):
+                output = max(output, nums[i])
+
+        return output
+
 
 # driver code
 nums = [-4,-2,1,4,8]
